dcc5eafb158799ad56b82c329c925352149b5060
Harshit Kumar <harshit.page@gmail.com>
2022-01-19 12:36:15 +0530

    Updates

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,9 @@ function slide()
 {
     var burger = document.querySelector('.burger');
     var links = document.querySelector('.links-popup');
+    var logo = document.querySelector('.fas');
+    var change = document.querySelector('.primal-li-con');
+    console.log(logo);
 
     burger.addEventListener('click', function(){
         links.style.display = 'block';
@@ -11,6 +14,20 @@ function slide()
     {
         links.style.display = 'none';
     })
+
+    if(burger.style.display == 'block')
+    {
+        logo.classList.remove('fa-4x');
+        logo.classList.add('fa-2x');
+        console.log('working the removing stuff');
+    }
+
+    if(burger.style.display == 'none')
+    {
+        logo.classList.remove('fa-2x');
+        logo.classList.add('fa-4x');
+    }
 }
 
 slide();
+console.log('working the main');
